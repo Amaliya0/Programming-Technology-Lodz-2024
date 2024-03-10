e7c9edbed1c41e4a4de8e612ad72f29f9dd91642
Amaliya0 <125629086+Amaliya0@users.noreply.github.com>
2024-03-10 22:56:31 +0100

    Added programm

---
+++ summation/summation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Input
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        //Sum
+        int SUM = num1 + num2;
+        //result
+        Console.WriteLine("Result of sum: " + SUM);
+    }
+}
